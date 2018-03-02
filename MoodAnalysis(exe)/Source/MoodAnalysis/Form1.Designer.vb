c99859a57a38e47f698b39fe8061f55e616be863
Hikky <umwdream2017@gmail.com>
2018-03-03 00:08:13 +0900

    Add exe Source(Visual Studio)

---
+++ MoodAnalysis(exe)/Source/MoodAnalysis/Form1.Designer.vb
@@ -0,0 +1,536 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+    'Windows フォーム デザイナーを使用して変更できます。  
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.NumericUpDown1 = New System.Windows.Forms.NumericUpDown()
+        Me.EvalMem = New System.Windows.Forms.Label()
+        Me.EvalNum1 = New System.Windows.Forms.TextBox()
+        Me.Number1 = New System.Windows.Forms.Label()
+        Me.Number2 = New System.Windows.Forms.Label()
+        Me.EvalNum2 = New System.Windows.Forms.TextBox()
+        Me.Number3 = New System.Windows.Forms.Label()
+        Me.EvalNum3 = New System.Windows.Forms.TextBox()
+        Me.Number4 = New System.Windows.Forms.Label()
+        Me.Number5 = New System.Windows.Forms.Label()
+        Me.Number6 = New System.Windows.Forms.Label()
+        Me.Number7 = New System.Windows.Forms.Label()
+        Me.Number8 = New System.Windows.Forms.Label()
+        Me.Number9 = New System.Windows.Forms.Label()
+        Me.Number10 = New System.Windows.Forms.Label()
+        Me.EvalNum4 = New System.Windows.Forms.TextBox()
+        Me.EvalNum5 = New System.Windows.Forms.TextBox()
+        Me.EvalNum6 = New System.Windows.Forms.TextBox()
+        Me.EvalNum7 = New System.Windows.Forms.TextBox()
+        Me.EvalNum8 = New System.Windows.Forms.TextBox()
+        Me.EvalNum9 = New System.Windows.Forms.TextBox()
+        Me.EvalNum10 = New System.Windows.Forms.TextBox()
+        Me.judge = New System.Windows.Forms.Button()
+        Me.GathAtt = New System.Windows.Forms.Label()
+        Me.NumericUpDown2 = New System.Windows.Forms.NumericUpDown()
+        Me.Score = New System.Windows.Forms.Label()
+        Me.lux = New System.Windows.Forms.Label()
+        Me.Noise = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Times = New System.Windows.Forms.Label()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.p1 = New System.Windows.Forms.Label()
+        Me.p2 = New System.Windows.Forms.Label()
+        Me.p3 = New System.Windows.Forms.Label()
+        Me.p4 = New System.Windows.Forms.Label()
+        Me.p5 = New System.Windows.Forms.Label()
+        Me.Mdp = New System.Windows.Forms.Label()
+        Me.p1md = New System.Windows.Forms.Label()
+        Me.p2md = New System.Windows.Forms.Label()
+        Me.p3md = New System.Windows.Forms.Label()
+        Me.p4md = New System.Windows.Forms.Label()
+        Me.p5md = New System.Windows.Forms.Label()
+        Me.mpmd = New System.Windows.Forms.Label()
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.NumericUpDown2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'NumericUpDown1
+        '
+        Me.NumericUpDown1.Location = New System.Drawing.Point(230, 15)
+        Me.NumericUpDown1.Name = "NumericUpDown1"
+        Me.NumericUpDown1.Size = New System.Drawing.Size(120, 31)
+        Me.NumericUpDown1.TabIndex = 0
+        Me.NumericUpDown1.Value = New Decimal(New Integer() {10, 0, 0, 0})
+        '
+        'EvalMem
+        '
+        Me.EvalMem.AutoSize = True
+        Me.EvalMem.Location = New System.Drawing.Point(15, 15)
+        Me.EvalMem.Name = "EvalMem"
+        Me.EvalMem.Size = New System.Drawing.Size(189, 24)
+        Me.EvalMem.TabIndex = 1
+        Me.EvalMem.Text = "Evaluator Member"
+        '
+        'EvalNum1
+        '
+        Me.EvalNum1.Location = New System.Drawing.Point(230, 125)
+        Me.EvalNum1.Name = "EvalNum1"
+        Me.EvalNum1.Size = New System.Drawing.Size(100, 31)
+        Me.EvalNum1.TabIndex = 2
+        '
+        'Number1
+        '
+        Me.Number1.AutoSize = True
+        Me.Number1.Location = New System.Drawing.Point(15, 125)
+        Me.Number1.Name = "Number1"
+        Me.Number1.Size = New System.Drawing.Size(54, 24)
+        Me.Number1.TabIndex = 3
+        Me.Number1.Text = "No.1"
+        '
+        'Number2
+        '
+        Me.Number2.AutoSize = True
+        Me.Number2.Location = New System.Drawing.Point(15, 185)
+        Me.Number2.Name = "Number2"
+        Me.Number2.Size = New System.Drawing.Size(54, 24)
+        Me.Number2.TabIndex = 4
+        Me.Number2.Text = "No.2"
+        '
+        'EvalNum2
+        '
+        Me.EvalNum2.Location = New System.Drawing.Point(230, 185)
+        Me.EvalNum2.Name = "EvalNum2"
+        Me.EvalNum2.Size = New System.Drawing.Size(100, 31)
+        Me.EvalNum2.TabIndex = 5
+        '
+        'Number3
+        '
+        Me.Number3.AutoSize = True
+        Me.Number3.Location = New System.Drawing.Point(15, 245)
+        Me.Number3.Name = "Number3"
+        Me.Number3.Size = New System.Drawing.Size(54, 24)
+        Me.Number3.TabIndex = 6
+        Me.Number3.Text = "No.3"
+        '
+        'EvalNum3
+        '
+        Me.EvalNum3.Location = New System.Drawing.Point(230, 245)
+        Me.EvalNum3.Name = "EvalNum3"
+        Me.EvalNum3.Size = New System.Drawing.Size(100, 31)
+        Me.EvalNum3.TabIndex = 7
+        '
+        'Number4
+        '
+        Me.Number4.AutoSize = True
+        Me.Number4.Location = New System.Drawing.Point(15, 305)
+        Me.Number4.Name = "Number4"
+        Me.Number4.Size = New System.Drawing.Size(54, 24)
+        Me.Number4.TabIndex = 8
+        Me.Number4.Text = "No.4"
+        '
+        'Number5
+        '
+        Me.Number5.AutoSize = True
+        Me.Number5.Location = New System.Drawing.Point(15, 365)
+        Me.Number5.Name = "Number5"
+        Me.Number5.Size = New System.Drawing.Size(54, 24)
+        Me.Number5.TabIndex = 9
+        Me.Number5.Text = "No.5"
+        '
+        'Number6
+        '
+        Me.Number6.AutoSize = True
+        Me.Number6.Location = New System.Drawing.Point(15, 425)
+        Me.Number6.Name = "Number6"
+        Me.Number6.Size = New System.Drawing.Size(54, 24)
+        Me.Number6.TabIndex = 10
+        Me.Number6.Text = "No.6"
+        '
+        'Number7
+        '
+        Me.Number7.AutoSize = True
+        Me.Number7.Location = New System.Drawing.Point(15, 485)
+        Me.Number7.Name = "Number7"
+        Me.Number7.Size = New System.Drawing.Size(54, 24)
+        Me.Number7.TabIndex = 11
+        Me.Number7.Text = "No.7"
+        '
+        'Number8
+        '
+        Me.Number8.AutoSize = True
+        Me.Number8.Location = New System.Drawing.Point(15, 545)
+        Me.Number8.Name = "Number8"
+        Me.Number8.Size = New System.Drawing.Size(54, 24)
+        Me.Number8.TabIndex = 12
+        Me.Number8.Text = "No.8"
+        '
+        'Number9
+        '
+        Me.Number9.AutoSize = True
+        Me.Number9.Location = New System.Drawing.Point(15, 605)
+        Me.Number9.Name = "Number9"
+        Me.Number9.Size = New System.Drawing.Size(54, 24)
+        Me.Number9.TabIndex = 13
+        Me.Number9.Text = "No.9"
+        '
+        'Number10
+        '
+        Me.Number10.AutoSize = True
+        Me.Number10.Location = New System.Drawing.Point(15, 665)
+        Me.Number10.Name = "Number10"
+        Me.Number10.Size = New System.Drawing.Size(66, 24)
+        Me.Number10.TabIndex = 14
+        Me.Number10.Text = "No.10"
+        '
+        'EvalNum4
+        '
+        Me.EvalNum4.Location = New System.Drawing.Point(230, 305)
+        Me.EvalNum4.Name = "EvalNum4"
+        Me.EvalNum4.Size = New System.Drawing.Size(100, 31)
+        Me.EvalNum4.TabIndex = 15
+        '
+        'EvalNum5
+        '
+        Me.EvalNum5.Location = New System.Drawing.Point(230, 365)
+        Me.EvalNum5.Name = "EvalNum5"
+        Me.EvalNum5.Size = New System.Drawing.Size(100, 31)
+        Me.EvalNum5.TabIndex = 16
+        '
+        'EvalNum6
+        '
+        Me.EvalNum6.Location = New System.Drawing.Point(230, 425)
+        Me.EvalNum6.Name = "EvalNum6"
+        Me.EvalNum6.Size = New System.Drawing.Size(100, 31)
+        Me.EvalNum6.TabIndex = 17
+        '
+        'EvalNum7
+        '
+        Me.EvalNum7.Location = New System.Drawing.Point(230, 485)
+        Me.EvalNum7.Name = "EvalNum7"
+        Me.EvalNum7.Size = New System.Drawing.Size(100, 31)
+        Me.EvalNum7.TabIndex = 18
+        '
+        'EvalNum8
+        '
+        Me.EvalNum8.Location = New System.Drawing.Point(230, 545)
+        Me.EvalNum8.Name = "EvalNum8"
+        Me.EvalNum8.Size = New System.Drawing.Size(100, 31)
+        Me.EvalNum8.TabIndex = 19
+        '
+        'EvalNum9
+        '
+        Me.EvalNum9.Location = New System.Drawing.Point(230, 605)
+        Me.EvalNum9.Name = "EvalNum9"
+        Me.EvalNum9.Size = New System.Drawing.Size(100, 31)
+        Me.EvalNum9.TabIndex = 20
+        '
+        'EvalNum10
+        '
+        Me.EvalNum10.Location = New System.Drawing.Point(230, 665)
+        Me.EvalNum10.Name = "EvalNum10"
+        Me.EvalNum10.Size = New System.Drawing.Size(100, 31)
+        Me.EvalNum10.TabIndex = 21
+        '
+        'judge
+        '
+        Me.judge.Location = New System.Drawing.Point(713, 690)
+        Me.judge.Name = "judge"
+        Me.judge.Size = New System.Drawing.Size(249, 57)
+        Me.judge.TabIndex = 22
+        Me.judge.Text = "Judge"
+        Me.judge.UseVisualStyleBackColor = True
+        '
+        'GathAtt
+        '
+        Me.GathAtt.AutoSize = True
+        Me.GathAtt.Location = New System.Drawing.Point(433, 15)
+        Me.GathAtt.Name = "GathAtt"
+        Me.GathAtt.Size = New System.Drawing.Size(234, 24)
+        Me.GathAtt.TabIndex = 23
+        Me.GathAtt.Text = "Gathering Attention by"
+        '
+        'NumericUpDown2
+        '
+        Me.NumericUpDown2.Location = New System.Drawing.Point(735, 15)
+        Me.NumericUpDown2.Name = "NumericUpDown2"
+        Me.NumericUpDown2.Size = New System.Drawing.Size(120, 31)
+        Me.NumericUpDown2.TabIndex = 24
+        '
+        'Score
+        '
+        Me.Score.AutoSize = True
+        Me.Score.Location = New System.Drawing.Point(15, 70)
+        Me.Score.Name = "Score"
+        Me.Score.Size = New System.Drawing.Size(181, 24)
+        Me.Score.TabIndex = 25
+        Me.Score.Text = "Score of Position"
+        '
+        'lux
+        '
+        Me.lux.AutoSize = True
+        Me.lux.Location = New System.Drawing.Point(435, 125)
+        Me.lux.Name = "lux"
+        Me.lux.Size = New System.Drawing.Size(162, 24)
+        Me.lux.TabIndex = 26
+        Me.lux.Text = "Illuminance(lux)"
+        '
+        'Noise
+        '
+        Me.Noise.AutoSize = True
+        Me.Noise.Location = New System.Drawing.Point(435, 185)
+        Me.Noise.Name = "Noise"
+        Me.Noise.Size = New System.Drawing.Size(103, 24)
+        Me.Noise.TabIndex = 27
+        Me.Noise.Text = "Noise(db)"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(735, 125)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 31)
+        Me.TextBox1.TabIndex = 28
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(735, 185)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(100, 31)
+        Me.TextBox2.TabIndex = 29
+        '
+        'Times
+        '
+        Me.Times.AutoSize = True
+        Me.Times.Location = New System.Drawing.Point(435, 245)
+        Me.Times.Name = "Times"
+        Me.Times.Size = New System.Drawing.Size(263, 24)
+        Me.Times.TabIndex = 30
+        Me.Times.Text = "Look up each other times"
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(735, 245)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(100, 31)
+        Me.TextBox3.TabIndex = 31
+        '
+        'p1
+        '
+        Me.p1.AutoSize = True
+        Me.p1.Location = New System.Drawing.Point(435, 365)
+        Me.p1.Name = "p1"
+        Me.p1.Size = New System.Drawing.Size(76, 24)
+        Me.p1.TabIndex = 32
+        Me.p1.Text = "p[1] = "
+        '
+        'p2
+        '
+        Me.p2.AutoSize = True
+        Me.p2.Location = New System.Drawing.Point(435, 425)
+        Me.p2.Name = "p2"
+        Me.p2.Size = New System.Drawing.Size(76, 24)
+        Me.p2.TabIndex = 33
+        Me.p2.Text = "p[2] = "
+        '
+        'p3
+        '
+        Me.p3.AutoSize = True
+        Me.p3.Location = New System.Drawing.Point(435, 485)
+        Me.p3.Name = "p3"
+        Me.p3.Size = New System.Drawing.Size(76, 24)
+        Me.p3.TabIndex = 34
+        Me.p3.Text = "p[3] = "
+        '
+        'p4
+        '
+        Me.p4.AutoSize = True
+        Me.p4.Location = New System.Drawing.Point(435, 545)
+        Me.p4.Name = "p4"
+        Me.p4.Size = New System.Drawing.Size(76, 24)
+        Me.p4.TabIndex = 35
+        Me.p4.Text = "p[4] = "
+        '
+        'p5
+        '
+        Me.p5.AutoSize = True
+        Me.p5.Location = New System.Drawing.Point(435, 605)
+        Me.p5.Name = "p5"
+        Me.p5.Size = New System.Drawing.Size(76, 24)
+        Me.p5.TabIndex = 36
+        Me.p5.Text = "p[5] = "
+        '
+        'Mdp
+        '
+        Me.Mdp.AutoSize = True
+        Me.Mdp.Location = New System.Drawing.Point(435, 665)
+        Me.Mdp.Name = "Mdp"
+        Me.Mdp.Size = New System.Drawing.Size(123, 24)
+        Me.Mdp.TabIndex = 37
+        Me.Mdp.Text = "MoodPoint "
+        '
+        'p1md
+        '
+        Me.p1md.AutoSize = True
+        Me.p1md.Location = New System.Drawing.Point(520, 365)
+        Me.p1md.Name = "p1md"
+        Me.p1md.Size = New System.Drawing.Size(0, 24)
+        Me.p1md.TabIndex = 38
+        '
+        'p2md
+        '
+        Me.p2md.AutoSize = True
+        Me.p2md.Location = New System.Drawing.Point(520, 425)
+        Me.p2md.Name = "p2md"
+        Me.p2md.Size = New System.Drawing.Size(0, 24)
+        Me.p2md.TabIndex = 39
+        '
+        'p3md
+        '
+        Me.p3md.AutoSize = True
+        Me.p3md.Location = New System.Drawing.Point(520, 485)
+        Me.p3md.Name = "p3md"
+        Me.p3md.Size = New System.Drawing.Size(0, 24)
+        Me.p3md.TabIndex = 40
+        '
+        'p4md
+        '
+        Me.p4md.AutoSize = True
+        Me.p4md.Location = New System.Drawing.Point(520, 545)
+        Me.p4md.Name = "p4md"
+        Me.p4md.Size = New System.Drawing.Size(0, 24)
+        Me.p4md.TabIndex = 41
+        '
+        'p5md
+        '
+        Me.p5md.AutoSize = True
+        Me.p5md.Location = New System.Drawing.Point(520, 605)
+        Me.p5md.Name = "p5md"
+        Me.p5md.Size = New System.Drawing.Size(0, 24)
+        Me.p5md.TabIndex = 42
+        '
+        'mpmd
+        '
+        Me.mpmd.AutoSize = True
+        Me.mpmd.Location = New System.Drawing.Point(585, 665)
+        Me.mpmd.Name = "mpmd"
+        Me.mpmd.Size = New System.Drawing.Size(0, 24)
+        Me.mpmd.TabIndex = 43
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(13.0!, 24.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(974, 759)
+        Me.Controls.Add(Me.mpmd)
+        Me.Controls.Add(Me.p5md)
+        Me.Controls.Add(Me.p4md)
+        Me.Controls.Add(Me.p3md)
+        Me.Controls.Add(Me.p2md)
+        Me.Controls.Add(Me.p1md)
+        Me.Controls.Add(Me.Mdp)
+        Me.Controls.Add(Me.p5)
+        Me.Controls.Add(Me.p4)
+        Me.Controls.Add(Me.p3)
+        Me.Controls.Add(Me.p2)
+        Me.Controls.Add(Me.p1)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.Times)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Noise)
+        Me.Controls.Add(Me.lux)
+        Me.Controls.Add(Me.Score)
+        Me.Controls.Add(Me.NumericUpDown2)
+        Me.Controls.Add(Me.GathAtt)
+        Me.Controls.Add(Me.judge)
+        Me.Controls.Add(Me.EvalNum10)
+        Me.Controls.Add(Me.EvalNum9)
+        Me.Controls.Add(Me.EvalNum8)
+        Me.Controls.Add(Me.EvalNum7)
+        Me.Controls.Add(Me.EvalNum6)
+        Me.Controls.Add(Me.EvalNum5)
+        Me.Controls.Add(Me.EvalNum4)
+        Me.Controls.Add(Me.Number10)
+        Me.Controls.Add(Me.Number9)
+        Me.Controls.Add(Me.Number8)
+        Me.Controls.Add(Me.Number7)
+        Me.Controls.Add(Me.Number6)
+        Me.Controls.Add(Me.Number5)
+        Me.Controls.Add(Me.Number4)
+        Me.Controls.Add(Me.EvalNum3)
+        Me.Controls.Add(Me.Number3)
+        Me.Controls.Add(Me.EvalNum2)
+        Me.Controls.Add(Me.Number2)
+        Me.Controls.Add(Me.Number1)
+        Me.Controls.Add(Me.EvalNum1)
+        Me.Controls.Add(Me.EvalMem)
+        Me.Controls.Add(Me.NumericUpDown1)
+        Me.Name = "Form1"
+        Me.Text = "MoodAnalysis"
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.NumericUpDown2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents NumericUpDown1 As NumericUpDown
+    Friend WithEvents EvalMem As Label
+    Friend WithEvents EvalNum1 As TextBox
+    Friend WithEvents Number1 As Label
+    Friend WithEvents Number2 As Label
+    Friend WithEvents EvalNum2 As TextBox
+    Friend WithEvents Number3 As Label
+    Friend WithEvents EvalNum3 As TextBox
+    Friend WithEvents Number4 As Label
+    Friend WithEvents Number5 As Label
+    Friend WithEvents Number6 As Label
+    Friend WithEvents Number7 As Label
+    Friend WithEvents Number8 As Label
+    Friend WithEvents Number9 As Label
+    Friend WithEvents Number10 As Label
+    Friend WithEvents EvalNum4 As TextBox
+    Friend WithEvents EvalNum5 As TextBox
+    Friend WithEvents EvalNum6 As TextBox
+    Friend WithEvents EvalNum7 As TextBox
+    Friend WithEvents EvalNum8 As TextBox
+    Friend WithEvents EvalNum9 As TextBox
+    Friend WithEvents EvalNum10 As TextBox
+    Friend WithEvents judge As Button
+    Friend WithEvents GathAtt As Label
+    Friend WithEvents NumericUpDown2 As NumericUpDown
+    Friend WithEvents Score As Label
+    Friend WithEvents lux As Label
+    Friend WithEvents Noise As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Times As Label
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents p1 As Label
+    Friend WithEvents p2 As Label
+    Friend WithEvents p3 As Label
+    Friend WithEvents p4 As Label
+    Friend WithEvents p5 As Label
+    Friend WithEvents Mdp As Label
+    Friend WithEvents p1md As Label
+    Friend WithEvents p2md As Label
+    Friend WithEvents p3md As Label
+    Friend WithEvents p4md As Label
+    Friend WithEvents p5md As Label
+    Friend WithEvents mpmd As Label
+End Class
---
+++ MoodAnalysis(exe)/Source/MoodAnalysis/Form1.vb
@@ -0,0 +1,113 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub judge_Click(sender As Object, e As EventArgs) Handles judge.Click
+        Dim moodpoint, p(5), x(5) As Double
+        Dim txtbox() As Windows.Forms.TextBox   'コントロールをまとめる
+        Dim txtlabel() As Windows.Forms.Label   '同上
+        Dim i, count As Integer
+        Dim GathAtt As Integer = NumericUpDown2.Value
+        Dim t, S As Double
+        Dim getmes As Boolean = True 'エラー時はムード値を求めない
+        txtbox = New TextBox() {Me.EvalNum1, Me.EvalNum2, Me.EvalNum3, Me.EvalNum4, Me.EvalNum5, Me.EvalNum6, Me.EvalNum7, Me.EvalNum8, Me.EvalNum9, Me.EvalNum10}
+        txtlabel = New Label() {Me.Number1, Me.Number2, Me.Number3, Me.Number4, Me.Number5, Me.Number6, Me.Number7, Me.Number8, Me.Number9, Me.Number10}
+        x(0) = 0
+        While txtbox(i).Visible '表示分を抽出
+            count += 1
+            i += 1
+            If i = 10 Then
+                Exit While
+            End If
+        End While
+        For i = 0 To count - 1
+            If Not txtbox(i).Text.Equals("") Then
+                x(0) += 1 / 5 * (Integer.Parse(txtbox(i).Text))
+            Else
+                MessageBox.Show("Please input a numerical value in Score textbox.")
+                getmes = False
+                Exit For
+            End If
+        Next i
+        x(0) /= count
+        p(0) = x(0) '美しさ指数
+        If Not (GathAtt = 0) Then
+            p(1) = GathAtt * -10000
+        Else
+            p(1) = 20
+        End If  '第三者指数
+        If Not TextBox1.Text.Equals("") Then
+            x(2) = Double.Parse(TextBox1.Text)
+        Else
+            MessageBox.Show("Please input a numerical value in Illuminance textbox.")
+            getmes = False
+        End If
+        p(2) = (1 / 5) * (104 - 2 * ((x(2) / 20) + (20 / x(2))))    '照度指数
+        If Not TextBox2.Text.Equals("") Then
+            x(3) = TextBox2.Text
+        Else
+            MessageBox.Show("Please input a numerical value in Noise textbox.")
+            getmes = False
+        End If
+        If (x(3) < 20 And x(3) >= 0) Then
+            p(3) = 20
+        ElseIf (x(3) < 70 And x(3) >= 20) Then
+            t = 100 - 2 * (x(3) - 20)
+            p(3) = 30 - (1000 / t)
+        Else
+            p(3) = -1 / 0
+        End If '静けさ指数
+        If Not TextBox2.Text.Equals("") Then
+            x(4) = TextBox3.Text
+        Else
+            MessageBox.Show("Please input a numerical value in Times textbox.")
+            getmes = False
+        End If
+        If (x(4) < 30 And x(4) > 0) Then
+            S = (100 / 30) * x(4)
+        ElseIf (x(4) <= 60 And x(4) >= 30) Then
+            S = 100
+        Else
+            S = 100 - 5 * (x(4) - 60)
+        End If
+        p(4) = (1 / 5) * S  '見つめ合い指数
+        For i = 0 To 4
+            moodpoint += p(i)
+        Next i
+        If getmes Then
+            p1md.Text = Math.Round(p(0), 2, MidpointRounding.AwayFromZero)
+            p2md.Text = Math.Round(p(1), 2, MidpointRounding.AwayFromZero)
+            p3md.Text = Math.Round(p(2), 2, MidpointRounding.AwayFromZero)
+            p4md.Text = Math.Round(p(3), 2, MidpointRounding.AwayFromZero)
+            p5md.Text = Math.Round(p(4), 2, MidpointRounding.AwayFromZero)
+            mpmd.Text = Math.Round(moodpoint, 2, MidpointRounding.AwayFromZero)
+        End If
+    End Sub
+
+    Private Sub NumericUpDown1_ValueChanged(sender As Object, e As EventArgs) Handles NumericUpDown1.ValueChanged
+        Dim txtbox() As Windows.Forms.TextBox
+        Dim txtlabel() As Windows.Forms.Label
+        Dim tf As Boolean
+        Dim i As Integer
+        Dim cell As Integer = NumericUpDown1.Value
+        NumericUpDown1.Minimum = 1
+        NumericUpDown1.Maximum = 10
+        txtbox = New TextBox() {Me.EvalNum1, Me.EvalNum2, Me.EvalNum3, Me.EvalNum4, Me.EvalNum5, Me.EvalNum6, Me.EvalNum7, Me.EvalNum8, Me.EvalNum9, Me.EvalNum10}
+        txtlabel = New Label() {Me.Number1, Me.Number2, Me.Number3, Me.Number4, Me.Number5, Me.Number6, Me.Number7, Me.Number8, Me.Number9, Me.Number10}
+        tf = txtbox(cell - 1).Visible
+        If tf Then
+            For i = cell To 9   '現数値から上は非表示
+                txtbox(i).Visible = False
+                txtlabel(i).Visible = False
+            Next i
+        Else
+            txtbox(cell - 1).Visible = True '現数値は表示
+            txtlabel(cell - 1).Visible = True
+        End If
+    End Sub
+
+    Private Sub NumericUpDown2_ValueChanged(sender As Object, e As EventArgs) Handles NumericUpDown2.ValueChanged
+        NumericUpDown2.Minimum = 0
+    End Sub
+End Class
